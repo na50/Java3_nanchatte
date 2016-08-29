9e16ae5a90c7c40ef22ddc0f93d46c3e2fcdfa19
unknown <>
2016-08-29 11:42:08 +0900

    ふぁーすとみっしょん

---
+++ src/UenoFirstMission.java
@@ -0,0 +1,18 @@
+
+package mymission;
+public class UenoFirstMission{
+// インスタンス変数
+private String name;
+// コンストラクタ
+public UenoFirstMission(){
+    name = "うえの";
+}
+// セッタ
+public void setName(String name){
+    this.name = name;
+}
+// ゲッタ
+public  String getName(){
+    return name;
+}
+}
