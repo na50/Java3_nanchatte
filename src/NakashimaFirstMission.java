1f606ecffe3fd6203961c6a553f80eb9c639fba7
unknown <>
2016-08-29 11:49:40 +0900

    作成

---
+++ src/NakashimaFirstMission.java
@@ -0,0 +1,17 @@
+package mymission;
+public class NakashimaFirstMission{
+// インスタンス変数
+private String name;
+// コンストラクタ
+public NakashimaFirstMission(){
+    name = "中島";
+}
+// セッタ
+public void setName(String name){
+    this.name = name;
+}
+// ゲッタ
+public  String getName(){
+    return name;
+}
+}
