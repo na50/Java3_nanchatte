04c00dd6366597830f69fd9fbb8c0afeb042d107
unknown <>
2016-08-29 11:31:14 +0900

    nago

---
+++ src/nagoFirstMission.java
@@ -0,0 +1,17 @@
+package mymission;
+public class nagoFirstMission{
+// インスタンス変数
+private String name;
+// コンストラクタ
+public nagoFirstMission(){
+    name = "名合";
+}
+// セッタ
+public void setName(String name){
+    this.name = name;
+}
+// ゲッタ
+public  String getName(){
+    return name;
+}
+}
